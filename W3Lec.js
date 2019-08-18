2a7e8ad34d1389eda220381bd4f6d1dee0aa174c
samhalsall23 <samhalsall23@gmail.com>
2019-08-18 22:25:30 +1000

    initial commit

---
+++ W3Lec.js
@@ -0,0 +1,63 @@
+//go back to 40min in to see github part, that is missing
+
+
+let express = require ('express'); // package from a third party
+
+let app = express();
+
+app.get("/", function (req, res){
+    res.send("Hello express app");
+});
+
+app.get("/wiki/:keyword/fit2095/:id",function (req, res){ // column means you are telling express anything after wiki is a parameter
+    console.log(req.url); // /wiki/space/fit2095/123
+    console.log(req.params); // { keyword: 'space', id: '123' }
+    
+});
+
+app.get("/report", function (req, res){
+    console.log(req.url);
+    console.log(req.params);
+    
+    
+    
+});
+
+
+app.get("/about", function (req, res){
+    res.send("About page");
+});
+app.get("/contactus", function (req, res){
+    res.send("Contact Us page");
+});
+
+app.post("/calc", function (req, res){
+    // your logic
+
+}); //get executed if post request arrives
+
+
+
+app.listen(8000);
+
+
+
+
+
+// let http = require ('http');
+
+// http.createServer(function (res, req){
+
+//     if (req.method === "GET"){
+//         // handle Get
+//         if (req.url ==="/"){
+//             //send back homepage
+//         } else if (req.url === "/about"){
+//             // send about page
+//         }
+//     } else if (req.method === "POST"){
+//         // handle Post
+//     }
+
+// }).listen(8000)
+
